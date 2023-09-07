19af7c43eee604bea03b706ba838addef2b0566e
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-09-06 22:34:37 -0500

    stuff added

--- in.js
+++ in.js
@@ -8,12 +8,9 @@ async function getSearch(search, page) {
     obj = new Object();
     obj.books = []
     
-    for( let x =1 ; x< page; x++){
-    const url = 'http://libgen.rs/search.php?req=$'+searchFilter+'&open=0&res=100&view=detailed&phrase=1&column=$title&page='+x.toString()
-    console.log(url)
+    const url = 'http://libgen.rs/search.php?req=$'+searchFilter+'&open=0&res=100&view=detailed&phrase=1&column=$title&page='+ page.toString()
     request(url, (error,response, html) => {
     if (!error && response.statusCode== 200) {
-        console.log("woring")
         const $ = cheerio.load (html);
         
         $('table').each((i, el)=>{
@@ -26,7 +23,6 @@ async function getSearch(search, page) {
                 let linkUnfiltered = $(el).find("td b a").attr("href")
                 const link = linkUnfiltered.replace(".." , "http://libgen.rs")
                 const img = "http://library.lol" + $(el).find('img').attr('src');
-                console.log(i,title)
                 obj.books.push({
                     "author" : author,
                     "title" : title,
@@ -35,25 +31,80 @@ async function getSearch(search, page) {
                     
                 })
             
+           }else{
+
            }
-           
-
         })
+        console.log(obj)
+        return []
 
+    
     }else{
-        return null       
+        return obj
     }
 
-     });
-    }
-    console.log(obj)
-    return obj
+   });
 
 }
 
-getSearch("middle school", 4)
+//getSearch("middle school", 1)
 
-async function detailed( url){
+async function getDetails(url){
+    obj = new Object();
+    obj.books = []
+    request(url, (error,response, html) => {
+    if (!error && response.statusCode== 200) {
+        console.log("woring")
+        const $ = cheerio.load (html);
+        
+        const title = $("body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3) > b:nth-child(1) > a:nth-child(1)").text()
+            const author = $("body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > b:nth-child(1)").text()
+            const details = $('body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(19) > td:nth-child(1)').text()
+            const download = $('body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(18) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)').attr('href')
+            const img = 'http://libgen.rs'+$('body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)').find('img').attr('src')
+            const url = $('body > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(18) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)').attr('href')
+            obj.books.push({
+                    "title" : title,
+                    "author" : author,
+                    "details": details,
+                    "image": img,
+                    "download" : download,
+                    
+                })
+        console.log(obj)
+        return []
+
+    
+    }else{
+        return obj
+    }
+
+   });
 
 }
-//https://stackoverflow.com/questions/8963693/how-to-create-json-string-in-javascript
+
+
+
+ async function download(url){
+    obj = new Object();
+    obj.books = []
+    request(url, (error,response, html) => {
+    if (!error && response.statusCode== 200) {
+        console.log("woring")
+        const $ = cheerio.load (html);
+        
+        const downlaodlink = $("#download > h2:nth-child(1) > a:nth-child(1)").attr("href")
+            obj.books.push({
+                'downlaodlink' :downlaodlink 
+                    
+                })
+        console.log(obj)
+        return []
+
+    
+    }else{
+        return obj
+    }});
+ }
+
+ download("http://library.lol/main/AC901C92D82255ED0570285C5FCEB0CB");
